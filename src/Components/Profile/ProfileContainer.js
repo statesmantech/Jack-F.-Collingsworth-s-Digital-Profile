b888372f859fa384375b9fa5afcdf1eb7fc4c612
Statesman Technologies, Inc <jfc@statesman.tech>
2017-07-10 14:41:37 -0700

    Bug fix.

--- src/Components/Profile/ProfileContainer.js
+++ src/Components/Profile/ProfileContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Image, Header } from 'semantic-ui-react'
+import { Image, Header, Grid } from 'semantic-ui-react'
 
 class ProfileContainer extends Component {
   render() {
